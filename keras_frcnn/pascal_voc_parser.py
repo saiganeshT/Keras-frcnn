48e5fb488dc6a600161246440a5b5f779336f421
a-gentili <55603257+a-gentili@users.noreply.github.com>
2022-05-06 09:00:07 +0200

    fixed initial image w/o obj

--- keras_frcnn/pascal_voc_parser.py
+++ keras_frcnn/pascal_voc_parser.py
@@ -50,6 +50,7 @@ def get_data(input_path, smth):
 		# test_files = os.listdir('/content/dataset/test')
 		
 		annots = [os.path.join(annot_path, s) for s in os.listdir(annot_path)]
+		annotation_data = {}
 		idx = 0
 		for annot in annots:
 			try:
